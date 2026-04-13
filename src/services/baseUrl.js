2779ff33e64660873686917f164e04eb74b2ea7d
Rajan Verma <vermarajan5032@gmail.com>
2026-04-13 19:55:05 +0530

    Update baseUrl.js

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,3 +1,6 @@
-const baseUrl = import.meta.env.VITE_API_URL || "https://pos-final-b-ooir.onrender.com/api";
+// baseUrl.js
+const baseUrl =
+  import.meta.env.VITE_API_URL ||
+  "https://pos-final-b-ooir.onrender.com/api";
 
 export default baseUrl;
